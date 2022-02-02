3878d1bf33b985ec0f2968a716d571812f495149
Alexey Y <yyexela@disroot.org>
2022-02-01 19:29:38 -0700

    Added OpenPIV files, modified directory structure

---
+++ FSSS/pivmat.m
@@ -0,0 +1,32 @@
+% Path of OpenPIV's output
+path = "openpiv.txt";
+
+% Load OpenPIV's output file
+dr = loadvec(path);
+
+% Adjust after import due formatting inconsistency (see DLA Report 9)
+dr.vy = swapcols(dr.vy);
+dr.vx = swapcols(dr.vx);
+
+% Calculating hp (effective water height)
+h0 = 8;     % top layer height (mm)
+np = 1.333; % top layer index of refraction
+            % (1.33 water 1.49 acrylic, 1.56 glass)
+hg = 5;     % bottom layer height
+ng = 1.56;  % bottom layer index of refraction
+
+hp = h0 + (np./ng).*hg;
+
+% Remove slope
+dr = removemean(dr);
+
+% Get FSSS Height and set the average height to what we expect
+h = surfheight(dr, hp, 890, np);
+h.w = adjustheight(h.w, h0);
+
+% Plot the height
+showf(h, 'Surf')
+%subplot(1,2,1), showf(h, 'Surf')
+%subplot(1,2,2), showf(h)
+
+colormap jet
---
+++ OpenPIV/ProjectionDistortion.m
@@ -0,0 +1,43 @@
+% For directories, ensure '/' is at the end
+Trial = "1";
+input_dir = "Trial " + Trial + "/Raw/";
+output_dir = "Trial " + Trial + "/Aligned/";
+extension = ".JPG";
+
+% This is the reference image for all other images
+ref_img_name = "IMG_0008";
+% This is the image we are transforming to fit ref_img_name
+trs_img_name = "IMG_0011";
+
+% Read images (ref = reference, trs = the one to transform)
+% Converts images to grayscale
+ref_in_path = convertStringsToChars(fullfile(input_dir, ref_img_name + extension));
+trs_in_path = convertStringsToChars(fullfile(input_dir, trs_img_name + extension));
+ref = rgb2gray(imread(ref_in_path));
+trs = rgb2gray(imread(trs_in_path));
+
+% If either mp or fp don't exist, then run cpselect
+% If both mp and fp exist, prompt the user for what to do
+if ~exist('mp','var') || ~exist('fp','var') || ~isempty(input('Press ENTER to skip feature selection\n>> ','s'))
+    % If mp and fp exist, load them
+    if exist('mp','var') && exist('fp','var') && ~isempty(mp) && ~isempty(fp)
+        [mp,fp] = cpselect(trs,ref,mp,fp,'Wait',true);
+    else
+        [mp,fp] = cpselect(trs,ref,'Wait',true);
+    end
+end
+
+% Infer geometric transformation
+t = fitgeotrans(mp,fp,'projective');
+
+% Transform trs Image
+Rfixed = imref2d(size(ref));
+reg = imwarp(trs,t,'OutputView',Rfixed);
+
+% Write to file
+ref_out_path = convertStringsToChars(fullfile(output_dir, ref_img_name + "_ref_prj" + extension));
+reg_out_path = convertStringsToChars(fullfile(output_dir, trs_img_name + "_reg_prj" + extension));
+imwrite(ref, ref_out_path)
+imwrite(reg, reg_out_path)
+
+imshowpair(ref,reg,'ColorChannels','red-cyan');
