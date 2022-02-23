94877a60604c549d4596b8e8c5285c95e64f68a5
yyexela <yyexela@disroot.org>
2022-02-23 14:23:46 -0700

    Changed test example for fsolve

--- FSSS/test.m
+++ FSSS/test.m
@@ -1,8 +1,5 @@
-fun = @root2d;
-x0 = [0,0];
-x = fsolve(fun,x0);
-
-function F = root2d(x)
-    F(1) = exp(-exp(-(x(1)+x(2)))) - x(2)*(1+x(1)^2);
-    F(2) = x(1)*cos(x(2)) + x(2)*sin(x(1)) - 0.5;
-end
+fun = @(x)x*x*x - [1,2;3,4];
+x0 = ones(2);
+options = optimoptions('fsolve','Display','off');
+[x,fval,exitflag,output] = fsolve(fun,x0,options)
+sum(sum(fval.*fval))
