bd3c2a60bc946346db7b29bdd1e7b6376c697e93
Alexey Y <yyexela@disroot.org>
2022-04-18 15:01:53 -0600

    Fixed bug in red_cyan.m

--- Image Preprocessing/red_cyan.m
+++ Image Preprocessing/red_cyan.m
@@ -1,15 +1,15 @@
 % For directories, ensure '/' is at the end
 %input_dir = "Outdoor_old/";
-input_dir_1 = "./Experiment Images/Cropped/";
-input_dir_2 = "./Experiment Images/Cropped/";
-extension = ".JPG";
+input_dir_1 = "../Experiment Images/Cropped/";
+input_dir_2 = "../Experiment Images/Cropped/";
+extension = ".png";
 
 % This is the reference image for all other images
 %still_img = "StillEdit";
-img_1 = "moving";
+img_1 = "water_drop_still_naligned";
 % This is the image we are transforming to fit still_img
 %moving_img = "MovingEdit";
-img_2 = "still";
+img_2 = "water_drop_moving_naligned";
 
 img_1_mat = imread(input_dir_1 + img_1 + extension);
 img_2_mat = imread(input_dir_2 + img_2 + extension);
