060d0fa9544d4db778d77ffe54bc7dcfa2684488
Alexey Y <yyexela@disroot.org>
2022-05-17 13:25:53 -0600

    Exp12 Image Registration

--- Image Preprocessing/ProjectionDistortion.m
+++ Image Preprocessing/ProjectionDistortion.m
@@ -1,25 +1,28 @@
+skip_option = 0;
+
 % For directories, ensure '/' is at the end
-dir = "../../Experiment 11/";
-trial = "trial 10/";
-input_dir = dir + trial + "raw/";
-output_dir = dir + trial + "aligned/";
+top_dir = "../../Experiment 12/";
+still_dir = top_dir + "trial 1/";
+trial = "11";
+seconds = "10";
+moving_dir = top_dir + "trial " + trial + "/";
 extension = ".JPG";
 
 % This is the reference image for all other images
-ref_img_name = "IMG_0133";
+ref_img_name = seconds;
 % This is the image we are transforming to fit ref_img_name
-trs_img_name = "IMG_0149";
+trs_img_name = seconds;
 
 % Read images (ref = reference, trs = the one to transform)
 % Converts images to grayscale
-ref_in_path = convertStringsToChars(fullfile(input_dir + "still", ref_img_name + extension));
-trs_in_path = convertStringsToChars(fullfile(input_dir + "moving", trs_img_name + extension));
+ref_in_path = convertStringsToChars(fullfile(still_dir + "raw/", ref_img_name + extension));
+trs_in_path = convertStringsToChars(fullfile(moving_dir + "raw/", trs_img_name + extension));
 ref = rgb2gray(imread(ref_in_path));
 trs = rgb2gray(imread(trs_in_path));
 
 % If either mp or fp don't exist, then run cpselect
 % If both mp and fp exist, prompt the user for what to do
-if ~exist('mp','var') || ~exist('fp','var') || ~isempty(input('Press ENTER to skip feature selection\n>> ','s'))
+if ~skip_option || ~exist('mp','var') || ~exist('fp','var') || ~isempty(input('Press ENTER to skip feature selection\n>> ','s'))
     % If mp and fp exist, load them
     if exist('mp','var') && exist('fp','var') && ~isempty(mp) && ~isempty(fp)
         [mp,fp] = cpselect(trs,ref,mp,fp,'Wait',true);
@@ -36,9 +39,12 @@ Rfixed = imref2d(size(ref));
 reg = imwarp(trs,t,'OutputView',Rfixed);
 
 % Write to file
-ref_out_path = convertStringsToChars(fullfile(output_dir + "still", ref_img_name + "_ref_nrs" + extension));
-reg_out_path = convertStringsToChars(fullfile(output_dir + "moving", trs_img_name + "_reg_nrs" + extension));
+ref_out_path = convertStringsToChars(fullfile(still_dir + "aligned/", ref_img_name + "_ref_nrs" + extension));
+reg_out_path = convertStringsToChars(fullfile(moving_dir + "aligned/", trs_img_name + "_reg_nrs" + extension));
 imwrite(ref, ref_out_path)
 imwrite(reg, reg_out_path)
 
 imshowpair(ref,reg,'ColorChannels','red-cyan');
+
+filename = seconds + "_" + trial;
+save(convertStringsToChars(filename))
